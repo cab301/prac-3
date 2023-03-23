c2ea9ccd6ab8bb2101ff1b830608abc395eefb8a
trand8 <trand8@MELT-GP-S1048>
2023-03-23 18:53:39 +1000

    Tutoritla 3

---
+++ Code/Code/IStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    internal interface IStack
+    {
+        /// <summary>
+        /// The size of the stack
+        /// </summary>
+        int Size { get; }
+        /// <summary>
+        /// If the stack is empty
+        /// </summary>
+        bool IsEmpty { get; }
+        /// <summary>
+        /// Add an element to the top of the stack
+        /// </summary>
+        /// <param name="element"></param>
+        void Push(double element);
+        /// <summary>
+        /// Return the element on the top of the stack
+        /// and removes it
+        /// </summary>
+        /// <returns>The element on top</returns>
+        double Pop();
+        /// <summary>
+        /// Return the element on the top of the stack
+        /// </summary>
+        /// <returns></returns>
+        double Peek();
+    }
+}
---
+++ Code/Question 4/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Dequeue();
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Enqueue " + i);
+
+                queue.Enqueue(i);
+                Console.WriteLine("Queue head: " + queue.Head());
+                Console.WriteLine("Queue tail: " + queue.Tail());
+            }
+            Console.WriteLine("Queue size = " + queue.Count);
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine("Queue head: " + queue.Head());
+                Console.WriteLine("Queue tail: " + queue.Tail());
+                Console.WriteLine("Dequeue " + queue.Dequeue());
+            }
+
+            Console.WriteLine("Queue size = " + queue.Count);
+
+            if (queue.IsEmpty())
+            {
+                Console.WriteLine("The queue is now empty");
+            }
+
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine("Enqueue " + i);
+                queue.Enqueue(i);
+            }
+
+            Console.WriteLine("Peeking at the head of the queue the element is " + queue.Head());
+            Console.ReadLine();
+        }
+    }
+}
