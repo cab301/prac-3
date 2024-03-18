a9d08eacf169459c35410b4befd4cb1cbe41c67e
Dan Tran <67458114+autumnssuns@users.noreply.github.com>
2024-03-18 14:50:41 +1000

    Add files via upload

---
+++ CustomerCollection/CustomerCollection.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCollection
+{
+    internal class CustomerCollection
+    {
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+
+        private Customer[] data;
+
+        public CustomerCollection(int capacity = 10)
+        {
+            Capacity = capacity;
+            Count = 0;
+            data = new Customer[Capacity];
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            data = new Customer[Capacity];
+        }
+
+        public void Insert(string firstName, string lastName, string phoneNumber)
+        {
+            Customer newCustomer = new Customer(firstName, lastName, phoneNumber);
+            Insert(newCustomer);
+        }
+
+        public void Insert(Customer customer)
+        {
+            if (IsFull()) return;
+            data[Count] = customer;
+            Count++;
+        }
+
+        public string Find(string firstName, string lastName)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Customer current = data[i];
+                if (current.FirstName.Equals(firstName)
+                    && current.LastName.Equals(lastName))
+                {
+                    return current.PhoneNumber;
+                }
+            }
+            return null;
+        }
+
+        public void Delete(string firstName, string lastName)
+        {
+            if (Find(firstName, lastName) == null) return;
+            // Find the index to delete
+            int index;
+            for (index = 0; index < Count; index++)
+            {
+                Customer current = data[index];
+                if (current.FirstName.Equals(firstName)
+                    && current.LastName.Equals(lastName))
+                {
+                    break;
+                }
+            }
+            // Shift everything after that index to the left
+            for (; index < Count - 1; index++)
+            {
+                data[index] = data[index + 1];
+            }
+            Count--;
+            //Delete(firstName, lastName);
+        }
+
+        public bool IsFull()
+        {
+            return Count == Capacity;
+        }
+
+        public void Display()
+        {
+            foreach (Customer customer in data)
+            {
+                Console.WriteLine(customer);
+            }
+        }
+    }
+}
---
+++ CustomerCollection/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace CustomerCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestInsert();
+            TestDelete();
+        }
+        
+        static void TestInsert()
+        {
+            CustomerCollection customers = new CustomerCollection(5);
+
+            // Case 1: Insert to empty collection
+            // Expected: One member gets displayed
+            Console.WriteLine("Insert to empty collection");
+            customers.Insert("Dan", "Tran", "0123456789");
+            customers.Display();
+
+            // Case 2: Insert to collection of one
+            Console.WriteLine("Insert to collection of one");
+            customers.Insert("Jone", "Smith", "0123456788");
+            customers.Display();
+
+            // Case 3: Insert to collection of many but not full
+            Console.WriteLine("Insert to collection of many (not full)");
+            customers.Insert("Jane", "Smith", "0123456787");
+            customers.Display();
+
+            // Case 4: Insert to full collection
+            Console.WriteLine("Insert to collection of many (not full)");
+            customers.Insert("Jim", "Smith", "0123456786");
+            customers.Insert("Jerry", "Smith", "0123456785");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Insert("James", "Smith", "0123456784");
+            Console.WriteLine("After");
+            customers.Display();
+        }
+
+        static void TestDelete()
+        {
+            CustomerCollection customers = new CustomerCollection(5);
+
+            // Delete from empty collection
+            Console.WriteLine("Remove from empty collection");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("Dan", "Tran");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove existing element from collection of one");
+            customers.Insert("Dan", "Tran", "0123456789");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("Dan", "Tran");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove non-existing element from collection of one");
+            customers.Insert("Dan", "Tran", "0123456789");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("Jone", "Smith");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove existing element from collection of many (not full)");
+            customers.Insert("Dan", "Tran", "0123456789");
+            customers.Insert("Jones", "Smith", "0123456786");
+            customers.Insert("Jim", "Smith", "0123456786");
+            customers.Insert("Jerry", "Smith", "0123456785");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("Dan", "Tran");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove non-existing element from collection of many (not full)");
+            customers = new CustomerCollection(5);
+            customers.Insert("Dan", "Tran", "0123456789");
+            customers.Insert("Jones", "Smith", "0123456786");
+            customers.Insert("Jim", "Smith", "0123456786");
+            customers.Insert("Jerry", "Smith", "0123456785");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("James", "Smith");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove existing element from full collection");
+            customers = new CustomerCollection(5);
+            customers.Insert("Dan", "Tran", "0123456789");
+            customers.Insert("Jones", "Smith", "0123456788");
+            customers.Insert("Jim", "Smith", "0123456787");
+            customers.Insert("Jerry", "Smith", "0123456786");
+            customers.Insert("James", "Smith", "0123456785");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("James", "Smith");
+            Console.WriteLine("After");
+            customers.Display();
+
+            Console.WriteLine("Remove non-existing element from full collection");
+            customers = new CustomerCollection(5);
+            customers.Insert("Dan", "Tran", "0123456789");
+            customers.Insert("Jones", "Smith", "0123456788");
+            customers.Insert("Jim", "Smith", "0123456787");
+            customers.Insert("Jerry", "Smith", "0123456786");
+            customers.Insert("James", "Smith", "0123456785");
+            Console.WriteLine("Before");
+            customers.Display();
+            customers.Delete("John", "Smith");
+            Console.WriteLine("After");
+            customers.Display();
+        }
+
+        static void TestFind()
+        {
+
+        }
+    }
+}
